b9463b5366cad846348d97941e704b832bfc03c6
Kamil Wrobel <wrobelk@live.com>
2019-04-26 19:15:57 -0400

    Sets up table view

--- FizzBuzzChallenge/ViewController/SecondVC.swift
+++ FizzBuzzChallenge/ViewController/SecondVC.swift
@@ -11,20 +11,37 @@ import UIKit
 class SecondVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
  
     
-
+    //MARK: - Outlets
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+    //MARK: - Properties
+    let numbers : [Int] = {
+        var numbers = [Int]()
+        for number in 1...1000 {
+            numbers.append(number)
+        }
+        return numbers
+    }()
+    
+    
+    
+    //MARK: - LifeCycle Methods
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        tableView.delegate = self
+        tableView.dataSource = self
     }
     
 
     //MARK: - TableView Data Source
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return numbers.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = UITableViewCell()
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "FizzCell" , for: indexPath) as? FizzCustomTVCell else { return UITableViewCell() }
+        cell.titleLabel.text = "\(numbers[indexPath.row])"
         
         return cell
     }
---
+++ FizzBuzzChallenge/Views/FizzCustomTVCell.swift
@@ -0,0 +1,28 @@
+//
+//  FizzCustomTVCell.swift
+//  FizzBuzzChallenge
+//
+//  Created by Kamil Wrobel on 4/26/19.
+//  Copyright © 2019 Kamil Wrobel. All rights reserved.
+//
+
+import UIKit
+
+class FizzCustomTVCell: UITableViewCell {
+    
+    //MARK: - Outlets
+    @IBOutlet weak var titleLabel: UILabel!
+    
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+    }
+
+}
