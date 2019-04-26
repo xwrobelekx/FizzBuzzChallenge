d0995df49f33166ee17d914da24ca8b16fa50867
Kamil Wrobel <wrobelk@live.com>
2019-04-26 19:22:28 -0400

    Creates the logic for fizz buzz

--- FizzBuzzChallenge/ViewController/SecondVC.swift
+++ FizzBuzzChallenge/ViewController/SecondVC.swift
@@ -41,9 +41,23 @@ class SecondVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "FizzCell" , for: indexPath) as? FizzCustomTVCell else { return UITableViewCell() }
-        cell.titleLabel.text = "\(numbers[indexPath.row])"
-        
+        cell.titleLabel.text = checkForBuzz(for: numbers[indexPath.row])
         return cell
+    }
+    
+    /// This method checks wheather the number is divisible by 3, or 5, or 3 and 5, and returns appropriate String, or number its self if all statement are false.
+    func checkForBuzz(for number: Int) -> String {
+
+        if number % 3 == 0 && number % 5 == 0 {
+            return "Fizz Buzz"
+        } else if number % 3 == 0 {
+            return "Fizz"
+        } else if number % 5 == 0 {
+            return "Buzz"
+        } else {
+            return "\(number)"
+        }
+        
     }
 
 
